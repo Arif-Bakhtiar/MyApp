243b0da328cbf14c978ff7dfe5697160513d3c02
M. M. Arif Bakhtiar <80150536+Arif-Bakhtiar@users.noreply.github.com>
2024-11-09 14:12:19 +0600

    Full project

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Data;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository<Product> products;
+        public ProductController(ApplicationDbContext context)
+        {
+            products = products = new Repository<Product>(context);
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await products.GetAllAsync());
+        }
+    }
+}
